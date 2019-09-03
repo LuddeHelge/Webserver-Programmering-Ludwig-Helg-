71a0ab762df42e7383ff1006d163b3c24c90d6cf
LuddeHelge <46518181+LuddeHelge@users.noreply.github.com>
2019-09-03 10:36:19 +0200

    Nya skiten

--- Meny.php
+++ Meny.php
@@ -1,8 +1,8 @@
 <nav>
 			<ul>
-				<li><a href="index.html">Start</a></li>
-				<li><a href="products.html">Produkter</a></li>
-				<li><a href="sida3.html">Varusida 2</a></li>
-				<li><a href="login.html">Logga in</a></li>
+				<li><a href="index.php">Start</a></li>
+				<li><a href="products.php">Produkter</a></li>
+				<li><a href="sida3.php">Varusida 2</a></li>
+				<li><a href="login.php">Logga in</a></li>
 			</ul>
 		</nav>
